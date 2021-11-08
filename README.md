641cea297f46d30369b9556e10ca731be515e3dc
mark953fps <markville953@gmail.com>
2021-11-08 16:54:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-table-h53d47
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-table-h53d47)
---
+++ src/app/app.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import moment from 'moment';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public searchForm: FormGroup;
+
+  public locationData: any[] = [
+    {
+      id: 'MB01',
+      activity: 's6d5f4sdf6545f',
+      curCode: 'HKD',
+      gamingDate: '19/08/2021',
+      shiftCode: 'D'
+    },
+    {
+      id: 'MB02',
+      activity: 'df54gdfg4d5fg',
+      curCode: 'HKD',
+      gamingDate: '20/08/2021',
+      shiftCode: 'B'
+    },
+    {
+      id: 'MB03',
+      activity: 'fd7df98g7',
+      curCode: 'HKD',
+      gamingDate: '21/08/2021',
+      shiftCode: 'C'
+    }
+  ];
+
+  shiftData: any[] = [{ id: 1, code: 'D' }, { id: 2, code: 'B' }];
+  filteredLocation: any[];
+  location: string;
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      location: ['', Validators.required],
+      gamingDate: [new Date(), Validators.required],
+      shiftType: ['', Validators.required]
+    });
+  }
+
+  filterLocation(event) {
+    this.filteredLocation = [];
+    for (let i = 0; i < this.locationData.length; i++) {
+      let location = this.locationData[i];
+      if (location.id.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+        this.filteredLocation.push(location);
+      }
+    }
+  }
+
+  onSelectLocation(event) {
+    let dateSplit = [];
+    dateSplit = event.gamingDate.split('/');
+
+    this.searchForm.controls.location.setValue(event.id);
+
+    setTimeout(() => {
+      this.searchForm.controls.gamingDate.setValue(new Date(`${dateSplit[1]}/${dateSplit[0]}/${dateSplit[2]}`));
+      let index = this.shiftData.findIndex(shif => shif.code === event.shiftCode);
+      index >= 0 ? this.searchForm.controls.shiftType.setValue(this.shiftData[index]) : this.searchForm.controls.shiftType.setValue(null);
+    }, 100);
+  }
+
+  onRetrieve() {
+    const params = {
+      location: this.searchForm.controls.location.value,
+      gamingDate: this.searchForm.controls.gamingDate.value,
+      shiftType: this.searchForm.controls.shiftType.value ? this.searchForm.controls.shiftType.value['code'] : null
+    };
+
+    console.log(params, 'on retrieve');
+  }
+}
